90b6bd2e30781ea74c14f56e60abc471e35b26be
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-12-16 11:59:45 +0100

    angular: input

--- tp/angular-app/src/app/hello-world/hello-world.component.ts
+++ tp/angular-app/src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-hello-world",
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./hello-world.component.scss"],
 })
 export class HelloWorldComponent implements OnInit {
+  @Input() name = "World";
+
   constructor() {}
 
   ngOnInit(): void {}
