323636bb743e9cc8afee691a9ad16f802ccf0dc3
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-12-17 11:36:52 +0100

    react: composant Hello

---
+++ tp/react-app/src/Hello.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+export default function(p) {
+  console.log(p);
+  return <h1>Hello {p.name}!</h1>;
+}
---
+++ tp/react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./Hello";
+import "./index.css";
+
+ReactDOM.render(<Hello nam="toto" />, document.getElementById("root"));
