91e79c316b1e25c0dee87980038856b1e6495a8f
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-12-17 17:07:50 +0100

    react: routage imbriqué

--- tp/react-app/src/Hello.jsx
+++ tp/react-app/src/Hello.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 
+
+function Hola () {
+	return <div>Hola</div>
+}
+
+function GutenTag() {
+	return <div>Guten tag</div>
+}
 
 export default function(p) {
   console.log(p);
-  return <h1>Hello {p.name}!</h1>;
+  return <div>
+		<ul>
+			<li>
+				<NavLink to="/hello" activeClassName="activeLink" exact>
+					Hola
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/hello/gutentag" activeClassName="activeLink">
+					Guten tag
+				</NavLink>
+			</li>
+		</ul>
+		<div>
+			<Route path="/hello" exact component={Hola} />
+			<Route path="/hello/gutentag" component={GutenTag} />
+		</div>
+	</div>;
 }
