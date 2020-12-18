e17f85d1cbeffc9bfd8a1e8a5272467b2963a1ad
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-12-18 12:00:52 +0100

    vue: composant Todo

--- tp/vue-app/src/App.vue
+++ tp/vue-app/src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-bind:msg="message" />
+    <Todo
+      v-for="t in todos"
+      v-bind:title="t.title"
+      v-bind:completed="t.completed"
+      v-bind:key="t.id"
+    />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Todo
+  },
+  data () {
+    return {
+      message: "coucou",
+      todos: []
+    }
+  },
+  mounted () {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(todos => this.todos = todos)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ tp/vue-app/src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      v-bind:checked="completed"
+    />
+    {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  // Définir les propriétés: soit en donnant sulement leur nom
+  // props: ['title', 'completed'],
+
+  // Soit en donnant aussi leur type
+  props: {
+    title: String,
+    completed: Boolean
+  }
+
+}
+</script>
